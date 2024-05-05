0d99d7e960ddb0be56896a331fa02b191eaa24b1
Quankun1211 <quan7ammm@gmail.com>
2024-05-05 10:08:31 +0700

    header

--- src/components/Project/ListItem.jsx
+++ src/components/Project/ListItem.jsx
@@ -4,7 +4,7 @@ import Api from '../..'
 const ListItem = () => {
   const datas = Api()
   return (
-    <div id="project" className="mx-auto my-20 flex items-center justify-center flex-col p-16">
+    <div id="project" className="mx-auto my-20 flex items-center justify-center flex-col max-[768px]:p-8 p-16">
       <h1 className="text-2xl my-16 max-[768px]:text-center font-medium">Let's explore a few of my projects</h1>
 
     <div className="grid grid-cols-3 max-[768px]:grid-cols-1 gap-4">
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,39 +3,41 @@ import AnchorLink from "react-anchor-link-smooth-scroll"
 
 const Header = ({handleTheme}) => {
   return (
-    <div className="navbar bg-base-100 shadow-2xl max-[768px]:justify-around fixed z-20">
-      <div className="flex-1">
+    <div className="navbar bg-base-100 shadow-2xl fixed z-20">
+      <div className="min-[768px]:flex-1 " >
         <AnchorLink href="#introduce">
           <a className="btn btn-ghost text-xl">Personal website</a>
         </AnchorLink>
       </div>
-
-      <div className="hidden toggle-config">
-        <Toggle handleTheme={handleTheme}/>
+      
+      <div className="ml-header">
+          <div className="hidden toggle-config">
+            <Toggle handleTheme={handleTheme}/>
+          </div>
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <li>
+                <AnchorLink href="#introduce">
+                  <a className="btn btn-ghost text-sm">About me</a>
+                </AnchorLink>
+              </li>
+              <li>
+                <AnchorLink href="#project">
+                  <a className="btn btn-ghost text-sm">My project</a>
+                </AnchorLink>
+              </li>
+              <li>
+                <AnchorLink href="#contact">
+                  <a className="btn btn-ghost text-sm">Contact</a>
+                </AnchorLink>
+              </li>
+            </ul>
+          </div>
       </div>
-
-      <div className="hidden dropdown">
-        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-        </div>
-        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-          <li>
-            <AnchorLink href="#introduce">
-              <a className="btn btn-ghost text-sm">About me</a>
-            </AnchorLink>
-          </li>
-          <li>
-            <AnchorLink href="#project">
-              <a className="btn btn-ghost text-sm">My project</a>
-            </AnchorLink>
-          </li>
-          <li>
-            <AnchorLink href="#contact">
-              <a className="btn btn-ghost text-sm">Contact</a>
-            </AnchorLink>
-          </li>
-        </ul>
-      </div>
+      
       
       
       <div className="flex-none hidden-header gap-2">
