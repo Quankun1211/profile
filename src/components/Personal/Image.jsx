2f509f0cf8452abf5f8bee17870cbbe6a432bd3f
Quankun1211 <quan7ammm@gmail.com>
2024-05-05 10:25:04 +0700

    avatar

--- src/components/Personal/Image.jsx
+++ src/components/Personal/Image.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import avatar from '../../assets/avatar.png'
+import avatar1 from '../../assets/avatar1.png'
 
 const Image = () => {
   return (
       <div className="avatar">
         <div className="w-96 h-full rounded drop-shadow-2xl">
-          <img src={avatar} className='drop-shadow-2xl' />
+          <img src={avatar1} className='drop-shadow-2xl' />
         </div>
       </div>
   )
--- src/components/Personal/Information.jsx
+++ src/components/Personal/Information.jsx
@@ -6,13 +6,13 @@ const Information = () => {
       <h1 className=" text-5xl max-[768px]:text-2xl flex items-center font-cursive font-bold">
         Nguyen Hong Quan
       </h1>
-      <p className="text-xl w-5/6 text-justify font-medium mt-5">Welcome to my home. I'm Quan, a web developer with a create vision and a knack for problem-solving. With a solid foudation in HTML, CSS and Javascript, some technology like ReactJs, NodeJs,...
+      <p className="text-xl max-[768px]:w-fit w-5/6 text-justify font-medium mt-5">Welcome to my home. I'm Quan, a web developer with a create vision and a knack for problem-solving. With a solid foudation in HTML, CSS and Javascript, some technology like ReactJs, NodeJs,...
       With a constant thirst for learning and staying abreast of the latest technologies, I'm always eager to tackle new challenges and push the boundaries of what's possible on the web.
       </p>
       <AnchorLink href="#detail">
         <p href="" className="text-xl text-teal-600 font-medium mt-5 w-5/6">Detail</p>
       </AnchorLink>
-      <p className="text-xl sm:flex sm:justify-center xl:block font-medium mt-5 w-5/6">
+      <p className="text-xl  max-[768px]:text-center max-[768px]:mb-6 sm:flex sm:justify-center xl:block font-medium mt-5 w-5/6">
         Let's discover more below
       </p>
     </div>
